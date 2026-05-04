5d5be8c5db6d38c4a82ddb10ee3e9602edfd18b2
allie10021002 <allie10021002@gmail.com>
2026-05-04 15:09:08 +0800

    Add files via upload

---
+++ myjs.js
@@ -0,0 +1,16 @@
+﻿
+window.onload= function(){
+            document.getElementById("nav_bar").innerHTML = `<nav>
+            <div class="container">
+            <a href="index.html" class="logo">白沙屯三尊媽祖</a>
+            <ul>
+                <li><a href="index.html">首頁</a></li>
+                <li><a href="about.html">活動行程</a></li>
+                <li><a href="courses.html">報名資訊</a></li>
+                <li><a href="tools.html">交通指南</a></li>
+                <li><a href="testimonials.html"></a></li>
+
+            </ul>
+        </div>
+    </nav>`;
+        }
